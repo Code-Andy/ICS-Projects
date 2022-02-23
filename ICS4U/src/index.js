5d06e5df1134c543e3042c200873eac691cc9e70
Andy Zhang <zhangandy915@gmail.com>
2022-02-23 17:33:44 -0500

    commit

--- ICS4U/src/index.js
+++ ICS4U/src/index.js
@@ -1,1 +1,39 @@
-console.log("hello");
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDoc } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyASZgii0k99yeGB1vaIxaXHkWAZgH4CmQU",
+  authDomain: "dot-test-1de86.firebaseapp.com",
+  projectId: "dot-test-1de86",
+  storageBucket: "dot-test-1de86.appspot.com",
+  messagingSenderId: "266594375880",
+  appId: "1:266594375880:web:07db376ec0107afe882dfd",
+  measurementId: "G-9VP86KXK57",
+};
+// Initialize Firebase
+initializeApp(firebaseConfig);
+const db = getFirestore();
+
+import {} from "firebase/firestore";
+
+const colRef = collection(db, "books");
+
+// get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+    // console.log(snapshot.docs)
+    let books = [];
+    snapshot.docs.forEach((doc) => {
+      books.push({ ...doc.data(), id: doc.id });
+    });
+    console.log(books);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
