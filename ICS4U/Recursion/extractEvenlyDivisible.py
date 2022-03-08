6f87ec801b901a842629e99623278b526f96efe2
Andy Zhang <zhangandy915@gmail.com>
2022-03-08 14:06:39 -0500

    commit

--- ICS4U/Recursion/extractEvenlyDivisible.py
+++ ICS4U/Recursion/extractEvenlyDivisible.py
@@ -11,23 +11,6 @@ If you ran extractEvenlyDivisible([1,9,3,4,3,6,7,8,9], 3) you would get a return
 
 a = [1, 9, 3, 4, 3, 6, 7, 8, 9]
 
-'''
-def extractEvenlyDivisible(array, divisor):
-    b = set()
-    def helper(arr):
-        if(len(arr) == 0):
-            return
-        elif ((arr[0] % divisor) == 0):
-            b.add(arr[0])
-            arr.pop(0)
-            return helper(arr)
-        else:
-            arr.pop(0)
-            return helper(arr)
-    helper(array)
-    return sorted(b)
-'''
-
 
 def extractEvenlyDivisible(array, divisor):
     def helper(arr, index):
