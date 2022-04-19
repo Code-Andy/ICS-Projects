7a6dfbd9a1ecdab7fedf2e1287f123a85be4a240
Andy Zhang <zhangandy915@gmail.com>
2022-04-19 14:44:16 -0400

    commit

--- ICS4U/UI/script.js
+++ ICS4U/UI/script.js
@@ -2,25 +2,8 @@ document
   .getElementById("startContainer")
   .addEventListener("click", clickToLogin);
 
-function clickToLogin() {
-  document.getElementById("welcome").innerHTML = "Login in with Google";
-  document.getElementById("welcome").style.fontSize = 35 + "px";
-  document.getElementById("welcome").style.fontStyle = "normal";
-  document.getElementById("welcome").style.fontWeight = 300;
-  document.getElementById("googleLogo").style.display = "flex";
-  document.getElementById("googleLogo").animate(
-    [
-      // keyframes
-      { opacity: 0 },
-      { opacity: 1 },
-    ],
-    {
-      // timing options
-      duration: 1000,
-    }
-  );
-
-  document.getElementById("welcome").animate(
+function fadeElement(elementName) {
+  document.getElementById(elementName).animate(
     [
       // keyframes
       { opacity: 0 },
@@ -32,3 +15,28 @@ function clickToLogin() {
     }
   );
 }
+
+function clickToLogin() {
+  document.getElementById("welcome").innerHTML = "Login in with Google";
+  document.getElementById("welcome").style.fontSize = 35 + "px";
+  document.getElementById("welcome").style.fontStyle = "normal";
+  document.getElementById("welcome").style.fontWeight = 500;
+  document.getElementById("googleLogo").style.display = "flex";
+
+  fadeElement("googleLogo");
+  fadeElement("welcome");
+}
+
+document.getElementById("ourProject").addEventListener("click", aboutPage);
+
+let aboutActive = false;
+
+function aboutPage() {
+  document.getElementById("aboutPage").style.display = "flex";
+  document.getElementById("homePage").style.display = "none";
+  aboutActive = true;
+}
+
+window.addEventListener("popstate", function (event) {
+  //Your code here
+});
