87b442b2a863ab2d94533e08e9792783a2e15701
Andy Zhang <zhangandy915@gmail.com>
2022-05-17 01:13:05 -0400

    commit

--- ICS4U/Algorithms/main.py
+++ ICS4U/Algorithms/main.py
@@ -1,11 +1,10 @@
-import time
 import random
-import copy
-from binarySearch import binarySearch
 import dataGen
 
+from timeit import timeit
 from insertionSort import insertionSort
 from linearSearch import linearSearch
+from binarySearch import binarySearch
 
 
 sizeLists = [5, 10, 100, 1000, 10000, 30000, 50000, 75000]
@@ -16,53 +15,66 @@ def runTimes(objectSize):
     linearTimesUnsorted = ""
     linearTimesSorted = ""
     binaryTimes = ""
-    for x in range(10):
-        tempList = dataGen.generateData(objectSize)
-        startTime = time.time()
-        insertionSort(tempList)
-        endTime = time.time()
-        timeDif = endTime - startTime
-        insertionTimes = insertionTimes + "," + timeDif
 
     for x in range(10):
         randomValue = random.randrange(0, objectSize)
-        tempList = dataGen.generateData(objectSize, randomValue)
-        startTime = time.time()
-        linearSearch(tempList)
-        endTime = time.time()
-        timeDif = endTime - startTime
-        linearTimesUnsorted = linearTimesUnsorted + "," + timeDif
+        tempList = dataGen.generateSearchData(objectSize, randomValue)
 
-    for x in range(10):
-        randomValue = random.randrange(0, objectSize)
-        tempList = dataGen.generateData(objectSize, randomValue)
-        sortList = insertionSort(tempList)
-        startTime = time.time()
-        linearSearch(sortList)
-        endTime = time.time()
-        timeDif = endTime - startTime
-        linearTimesSorted = linearTimesSorted + "," + timeDif
+        timeDif = timeit(lambda: linearSearch(
+            tempList, randomValue), number=1)
+        linearTimesUnsorted = linearTimesUnsorted + "," + str(timeDif)
+
+        print(f"linear {x}")
+
+        timeDif = timeit(lambda: insertionSort(tempList), number=1)
+        insertionTimes = insertionTimes + "," + str(timeDif)
+
+        print(f"insertion {x}")
+
+        timeDif = timeit(lambda: linearSearch(
+            tempList, randomValue), number=1)
+        linearTimesSorted = linearTimesSorted + "," + str(timeDif)
+
+        print(f"sortedlinear {x}")
+
+        timeDif = timeit(lambda: binarySearch(
+            tempList, randomValue), number=1)
+        binaryTimes = binaryTimes + "," + str(timeDif)
+
+        print(f"binary {x}")
+
+    insertionTimes = insertionTimes[1:]
+    linearTimesUnsorted = linearTimesUnsorted[1:]
+    linearTimesSorted = linearTimesSorted[1:]
+    binaryTimes = binaryTimes[1:]
+
+    f.write(f"{objectSize} objects\n")
+    f.write(f"{insertionTimes}\n")
+    f.write(f"{linearTimesUnsorted}\n")
+    f.write(f"{linearTimesSorted}\n")
+    f.write(f"{binaryTimes}\n")
+    f.write("\n")
 
 
-def main():
-
-    startTime = time.time()
-    endTime = time.time()
-
-    print("\n\nThis took " + str(endTime - startTime) + " s")
+with open('times2.txt', 'w') as f:
+    for items in sizeLists:
+        print(items)
+        runTimes(items)
 
 
-insertionList = dataGen.generateData(100)
+# Demo of working sorts and printing first and last 20 elements in insertionSort()
 
-print(insertionList)
-print(insertionSort(insertionList)[0:20])
-print(insertionSort(insertionList)[-20:])
+# insertionList = dataGen.generateData(100)
 
-linearList = dataGen.generateSearchData(100, 50)
-print(linearList)
-print(linearSearch(linearList, 50))
-print(linearSearch(insertionSort(linearList), 50))
+# print(insertionList)
+# print(insertionSort(insertionList)[0:20])
+# print(insertionSort(insertionList)[-20:])
 
-binaryList = dataGen.generateSearchData(200, 21)
-print(binaryList)
-print(binarySearch(insertionSort(binaryList), 21))
+# linearList = dataGen.generateSearchData(100, 50)
+# print(linearList)
+# print(linearSearch(linearList, 50))
+# print(linearSearch(insertionSort(linearList), 50))
+
+# binaryList = dataGen.generateSearchData(200, 21)
+# print(binaryList)
+# print(binarySearch(insertionSort(binaryList), 21))
--- ICS4U/Algorithms/test.py
+++
@@ -1,3 +0,0 @@
-h = "je"
-h = h, "hello"
-print(h)
