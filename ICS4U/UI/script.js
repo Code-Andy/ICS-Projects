0256b8381ec65171ffbc93056b9cddbf5b17e8df
Andy Zhang <zhangandy915@gmail.com>
2022-06-01 19:25:51 -0400

    commit

--- ICS4U/UI/script.js
+++ ICS4U/UI/script.js
@@ -1,67 +1,51 @@
 //npm to setup webpack firebase and tiltjs
 
-// document
-//   .getElementById("startContainer")
-//   .addEventListener("click", clickToLogin);
+document
+  .getElementById("startContainer")
+  .addEventListener("click", clickToLogin);
 
-// function fadeElement(elementName) {
-//   document.getElementById(elementName).animate(
-//     [
-//       // keyframes
-//       { opacity: 0 },
-//       { opacity: 1 },
-//     ],
-//     {
-//       // timing options
-//       duration: 1000,
-//     }
-//   );
-// }
-
-// function clickToLogin() {
-//   document.getElementById("welcome").innerHTML = "Login in with Google";
-//   document.getElementById("welcome").style.fontSize = 35 + "px";
-//   document.getElementById("welcome").style.fontStyle = "normal";
-//   document.getElementById("welcome").style.fontWeight = 500;
-//   document.getElementById("googleLogo").style.display = "flex";
-
-//   fadeElement("googleLogo");
-//   fadeElement("welcome");
-// }
-
-// let activePage = "homePage";
-// let lastActive = "";
-
-// document.getElementById("ourProject").addEventListener("click", aboutPage);
-
-// document.getElementById("dotLogo").addEventListener("click", homePage);
-
-// function aboutPage() {
-//   document.getElementById("aboutPage").style.display = "flex";
-//   document.getElementById("homePage").style.display = "none";
-//   lastActive = activePage;
-//   activePage = "aboutPage";
-// }
-
-// function homePage() {
-//   document.getElementById("aboutPage").style.display = "none";
-//   document.getElementById("homePage").style.display = "flex";
-//   lastActive = activePage;
-//   activePage = "homePage";
-// }
-
-fetch("./questions.json")
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((error) => console.log(error));
-
-document.getElementById("yes").addEventListener("click", yesClick);
-
-let questionNumber = 1;
-
-function yesClick() {
-  questionNumber += 1;
-  console.log(questionNumber);
+function fadeElement(elementName) {
+  document.getElementById(elementName).animate(
+    [
+      // keyframes
+      { opacity: 0 },
+      { opacity: 1 },
+    ],
+    {
+      // timing options
+      duration: 1000,
+    }
+  );
 }
 
-document.getElementById("questionText").innerHTML = "q" + questionNumber;
+function clickToLogin() {
+  document.getElementById("welcome").innerHTML = "Login in with Google";
+  document.getElementById("welcome").style.fontSize = 35 + "px";
+  document.getElementById("welcome").style.fontStyle = "normal";
+  document.getElementById("welcome").style.fontWeight = 500;
+  document.getElementById("googleLogo").style.display = "flex";
+
+  fadeElement("googleLogo");
+  fadeElement("welcome");
+}
+
+let activePage = "homePage";
+let lastActive = "";
+
+document.getElementById("ourProject").addEventListener("click", aboutPage);
+
+document.getElementById("dotLogo").addEventListener("click", homePage);
+
+function aboutPage() {
+  document.getElementById("aboutPage").style.display = "flex";
+  document.getElementById("homePage").style.display = "none";
+  lastActive = activePage;
+  activePage = "aboutPage";
+}
+
+function homePage() {
+  document.getElementById("aboutPage").style.display = "none";
+  document.getElementById("homePage").style.display = "flex";
+  lastActive = activePage;
+  activePage = "homePage";
+}
